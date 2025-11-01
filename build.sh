e21263fb92b73425ccccd65e05b6c04a250e2f2c
Yasuo-Higano <rakka_ccf@hotmail.com>
2025-11-01 21:26:22 +0900

    update image base

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 #npm run compile
 #npm run postinstall
 
-npx vsce package
+#npx vsce package
+npx vsce package --baseImagesUrl https://raw.githubusercontent.com/Yasuo-Higano/vscode-lisp-sxpr-highlighting/main/
